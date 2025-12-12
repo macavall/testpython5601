b4008d1b0fd0a26ccd491b0a980a1e0682e6f6c0
Matt <macavall@microsoft.com>
2025-12-12 15:22:25 -0600

    first

--- function_app.py
+++ function_app.py
@@ -25,3 +25,27 @@ def http1(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
+# An HTTP-Triggered Function with a Durable Functions Client binding
+@app.route(route="orchestrators/{functionName}")
+@app.durable_client_input(client_name="client")
+async def durable1_starter(req: func.HttpRequest, client):
+    function_name = req.route_params.get('functionName')
+    instance_id = await client.start_new(function_name)
+    response = client.create_check_status_response(req, instance_id)
+    return response
+
+
+# Orchestrator
+@app.orchestration_trigger(context_name="context")
+def durable1_orchestrator(context):
+    result1 = yield context.call_activity("durable1_activity", "Seattle")
+    result2 = yield context.call_activity("durable1_activity", "Tokyo")
+    result3 = yield context.call_activity("durable1_activity", "London")
+
+    return [result1, result2, result3]
+
+# Activity
+@app.activity_trigger(input_name="city")
+def durable1_activity(city: str):
+    return "Hello " + city 
